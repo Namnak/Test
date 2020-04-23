0365f05426d1ba1f2916a5de47106d135200a333
Jan Lautenschlaeger <jan.lautenschlaeger1996@googlemail.com>
2020-04-23 17:40:28 +0200

    Add working stuff

--- OpenGL/TestOcatve.m
+++ OpenGL/TestOcatve.m
@@ -1,11 +1,6 @@
-printf("Hello, World!\n");
+a = 13;
 
 disp("Hello, World!");
-disp("Hello, World!6");
-disp("Hello, World!4");
-disp("Hello, World!3");
 
-printf("Hello, World!\n");
-printf("Hello, World!\n");
-printf("Hello, World!\n");
-printf("Hello, World!\n");
+disp(a);
+disp(" - - END - - ");
